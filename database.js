f5408df87c60528787cd2455ee226b48e1c238fa
Christian Mock <chrm666@gmail.com>
2024-06-04 17:34:14 -0500

    databseconection

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',           // Usualmente 'localhost' para desarrollo local
+  host: 'monorail.proxy.rlwy.net',           // Usualmente 'localhost' para desarrollo local
   user: 'root',          // Tu nombre de usuario de MySQL
-  database: 'aire_pilates', // El nombre de tu base de datos
-  password: ''    // Tu contraseña de MySQL
+  database: 'railway', // El nombre de tu base de datos
+  password: 'rYINgliljKscMNNaWvuFzWkYMhKtbjXh'    // Tu contraseña de MySQL
 });
 
 module.exports = pool.promise();
