3fd0794cb76f990fe3520026f6b39eafa1be5101
Christian Mock <chrm666@gmail.com>
2024-06-04 18:52:15 -0500

    actualizacion db 5

--- database.js
+++ database.js
@@ -5,13 +5,17 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectTimeout: 10000 // Ajuste de tiempo de espera en milisegundos
+  connectTimeout: 10000 // Aumentar el tiempo de espera a 20 segundos
 });
 
 // Probar la conexión al iniciar la aplicación
 pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error de conexión a la base de datos:', err);
+    // Mensajes adicionales para diagnóstico
+    if (err.code === 'ETIMEDOUT') {
+      console.error('Error: La conexión ha superado el tiempo de espera. Verifica la red y el servidor de la base de datos.');
+    }
   } else {
     console.log('Conexión exitosa a la base de datos');
     connection.release(); // Liberar la conexión si es exitosa
