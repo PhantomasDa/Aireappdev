630c7e5ac1d215d79a6a4362cd72ad7b722128f2
Christian Mock <chrm666@gmail.com>
2024-06-04 18:21:40 -0500

    ajustes db2

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectTimeout: 10000 // Ajuste de tiempo de espera en milisegundos
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectTimeout: 10000 // Ajuste de tiempo de espera en milisegundos
 });
 
 module.exports = pool.promise();
